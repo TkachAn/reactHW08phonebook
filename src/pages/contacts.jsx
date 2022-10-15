52b326eb85464052f8da23d2318588433db0961e
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-15 10:28:20 +0300

    Update contacts.jsx

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -1,32 +1,40 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { ContactForm } from './contactForm/form';
-import { ContactList } from './contactList/list';
-import { Filter } from './filter/filter';
-import { selectContacts } from 'redux/selectors';
+import { ContactForm } from '../components/contactForm/form';
+import { ContactList } from '../components/contactList/list';
+import { Filter } from '../components/filter/filter';
+import { selectContacts } from '../redux/contacts/selectors';
 import { useEffect } from 'react';
-import { fetchContacts } from 'redux/operations';
-import css from './App.module.css';
+import { fetchContacts } from '../redux/contacts/operations';
+import css from '../components/App.module.css';
+import { Helmet } from 'react-helmet';
 
-export const ContactsPage = () => {
+export default function ContactsPage() {
   const dispatch = useDispatch();
-  const { contacts, isLoading } = useSelector(selectContacts);
 
+  const { contacts, isLoading } = useSelector(selectContacts);
+  // console.log({ contacts, isLoading });
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
   return (
-    <div className={css.container}>
+    <main className={css.container}>
+      <Helmet>
+        <title>Contacts</title>
+      </Helmet>
       <h1>Phonebook</h1>
-      <ContactForm contacts={contacts} />
+      <ContactForm />
       <h2>Contacts</h2>
       {Number(contacts.length) > 1 && <Filter />}
+
       {isLoading && <p>Loading contacts...</p>}
       {contacts.length > 0 ? (
         <ContactList />
       ) : (
         <p>Your phonebook is empty. Please add contact.</p>
       )}
-    </div>
+    </main>
   );
-};
+}
+//contacts={contacts}
+// export default ContactsPage;
