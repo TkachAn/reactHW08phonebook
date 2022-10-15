f65815927780fc2510425766cc7f3aa8fdc93117
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-15 10:27:17 +0300

    Create appBar.jsx

---
+++ src/components/forPages/appBar.jsx
@@ -0,0 +1,18 @@
+import Navigation from '../forPages/navigation';
+import { UserMenu } from '../forPages/userMenu';
+import { AuthNav } from '../forPages/authNav';
+import { useAuth } from '../../hooks/useAuth';
+import css from './css.module.css';
+
+export default function AppBar() {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <header className={css.header}>
+      <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    </header>
+  );
+}
+// className={css.header}
+//<UserMenu /> : <AuthNav />
