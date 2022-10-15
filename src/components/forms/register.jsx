3b35e7bac246a5a81698c6d2a6ccd3edb1f07b36
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-15 10:27:26 +0300

    Create register.jsx

---
+++ src/components/forms/register.jsx
@@ -0,0 +1,38 @@
+import { useDispatch } from 'react-redux';
+import { register } from '../../redux/auth/operations';
+import css from './form.module.css';
+
+export const RegisterForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    dispatch(
+      register({
+        name: form.elements.name.value,
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    );
+    form.reset();
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
+      <label className={css.label}>
+        Username
+        <input className={css.input} type="text" name="name" />
+      </label>
+      <label className={css.label}>
+        Email
+        <input className={css.input} type="email" name="email" />
+      </label>
+      <label className={css.label}>
+        Password
+        <input className={css.input} type="password" name="password" />
+      </label>
+      <button type="submit">Register</button>
+    </form>
+  );
+};
