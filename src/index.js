2fdc11549a58c15a6d1f598cd5647f52c818ec39
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-15 11:06:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/tkachan.github.io">
+        <BrowserRouter basename="">
           <App />
         </BrowserRouter>
       </PersistGate>
