564db823e0add9aa31ca2d3ee970efb287f4f1ff
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-15 22:37:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="">
+        <BrowserRouter basename="https://TkachAn.github.io/reactHW08phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
