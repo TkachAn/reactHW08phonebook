a30d77b37f8c50db07303ec62323b20c4326ae42
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-15 10:28:14 +0300

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,13 +1,14 @@
 import { Helmet } from 'react-helmet';
-import { LoginForm } from 'components/login/form';
+import { LoginForm } from '../components/forms/login';
 
 export default function Login() {
   return (
-    <div>
+    <main>
+      <h3>Login</h3>
       <Helmet>
         <title>Login</title>
       </Helmet>
       <LoginForm />
-    </div>
+    </main>
   );
 }
