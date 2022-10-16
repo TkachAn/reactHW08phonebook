ab18fcab242dc2c171e2e1db5bffaa9cf5ff478a
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-16 19:35:40 +0300

    Update appBar.jsx

--- src/components/forPages/appBar.jsx
+++ src/components/forPages/appBar.jsx
@@ -2,18 +2,20 @@ import Navigation from '../forPages/navigation';
 import { UserMenu } from '../forPages/userMenu';
 import { AuthNav } from '../forPages/authNav';
 import { useAuth } from '../../hooks/useAuth';
-// import { AppBar } from '@mui/material';
+import { AppBar } from '@mui/material';
 
 import css from './css.module.css';
 
-export default function AppBar() {
+export default function Bar() {
   const { isLoggedIn } = useAuth();
 
   return (
+    // <AppBar position="static">
     <header className={css.header}>
       <Navigation />
       {isLoggedIn ? <UserMenu /> : <AuthNav />}
     </header>
+    // {/* </AppBar> */}
   );
 }
 // className={css.header}
