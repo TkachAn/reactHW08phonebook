801778402b16ff0ed8f8a376cc0714e9daf052b2
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-16 19:35:44 +0300

    Update form.jsx

--- src/components/contactForm/form.jsx
+++ src/components/contactForm/form.jsx
@@ -1,6 +1,16 @@
 import { addContact } from '../../redux/contacts/operations';
 import { useDispatch } from 'react-redux';
 import css from './form.module.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const theme = createTheme();
 
 export function ContactForm() {
   const dispatch = useDispatch();
@@ -22,37 +32,93 @@ export function ContactForm() {
   };
   /** */
   return (
-    <form className={css.form} onSubmit={handleSubmit}>
-      <div className={css.data}>
-        <label className={css.label}>
-          Name
-          <input
-            className={css.input}
-            type="text"
-            name="name"
-            placeholder="enter your name, please."
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <label className={css.label}>
-          Phone number
-          <input
-            className={css.input}
-            type="tel"
-            name="phone"
-            placeholder="enter your phone number, please."
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </label>
-      </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="name"
+                    required
+                    fullWidth
+                    id="name"
+                    label="Name"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="phone"
+                    label="Phone number"
+                    name="phone"
+                    autoComplete="phone"
+                  />
+                </Grid>
 
-      <button className={css.btn} type="submit">
-        Add contact
-      </button>
-    </form>
+                <Grid item xs={12}></Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
+      {/* <form className={css.form} onSubmit={handleSubmit}>
+        <div className={css.data}>
+          <label className={css.label}>
+            Name
+            <input
+              className={css.input}
+              type="text"
+              name="name"
+              placeholder="enter your name, please."
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label className={css.label}>
+            Phone number
+            <input
+              className={css.input}
+              type="tel"
+              name="phone"
+              placeholder="enter your phone number, please."
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+        </div>
+
+        <button className={css.btn} type="submit">
+          Add contact
+        </button>
+      </form> */}
+    </>
   );
 }
