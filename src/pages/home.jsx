8da1b21329d715ca06e985f39ce122390cae59c3
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-16 21:54:05 +0300

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -16,14 +16,15 @@ import { Helmet } from 'react-helmet';
 export default function HomePage() {
   return (
     <main>
+      <Helmet>
+        <title>Phonebook</title>
+      </Helmet>
       <h3>Home</h3>
 
       <span role="img" aria-label="Greeting icon">
         💁‍♀️
       </span>
-      <Helmet>
-        <title>Phonebook</title>
-      </Helmet>
+
       <b>Contacts home page</b>
     </main>
   );
