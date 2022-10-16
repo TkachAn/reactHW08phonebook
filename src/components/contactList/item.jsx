1c3535ea73a28ffacff4dc5ae330538d8a544031
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-17 02:58:20 +0300

    Update item.jsx

--- src/components/contactList/item.jsx
+++ src/components/contactList/item.jsx
@@ -1,23 +1,39 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { delContact } from 'redux/contacts/operations';
-import css from './list.module.css';
+
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
 export const ContactItem = ({ id, name, number }) => {
   const dispatch = useDispatch();
   const delContacts = () => dispatch(delContact(id));
 
   return (
-    <li className={css.item} key={id}>
-      <div className={css.info}>
-        {name}: {number}
-        <button
-          className={css.btn}
-          type="button"
-          onClick={() => delContacts(id)}
-        >
-          X
-        </button>
-      </div>
+    <li key={id}>
+      <Container component="box">
+        <Box>
+          <Grid container spacing={1}>
+            <Grid item xs={12} sm={5}>
+              {name}:
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              {number}
+            </Grid>
+            <Grid item xs={12} sm={1}>
+              <Button
+                type="button"
+                onClick={() => delContacts(id)}
+                sx={{ color: 'red', fontWeight: 900 }}
+              >
+                X
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
     </li>
   );
 };
@@ -27,3 +43,9 @@ ContactItem.propType = {
   name: PropTypes.string.isRequired,
   number: PropTypes.string.isRequired,
 };
+// maxWidth={}
+//className={css.item}
+//className={css.btn}
+//hover {
+// color: white,
+//  backgroundColor: red, }
