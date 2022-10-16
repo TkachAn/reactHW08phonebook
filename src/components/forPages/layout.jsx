cad572255a670b7f2be3d748a9cb9762f9060cac
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-16 19:35:31 +0300

    Update layout.jsx

--- src/components/forPages/layout.jsx
+++ src/components/forPages/layout.jsx
@@ -1,16 +1,14 @@
 import { Outlet } from 'react-router-dom';
-// import { Toaster } from 'react-hot-toast';
-import AppBar from './appBar';
+import Bar from './appBar';
 import { Suspense } from 'react';
 
 export default function Layout() {
   return (
     <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
-      <AppBar />
+      <Bar />
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
-      {/* <Toaster position="top-right" reverseOrder={false} /> */}
     </div>
   );
 }
