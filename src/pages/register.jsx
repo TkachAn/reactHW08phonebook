fa55ec51d8a2e7076ccbf96e32a65ce30be2d481
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-17 02:57:31 +0300

    Update register.jsx

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -4,11 +4,10 @@ import SignUp from 'components/forms/singUp';
 export default function Register() {
   return (
     <main>
-      {/* <h3>Registration</h3> */}
       <Helmet>
         <title>Registration</title>
       </Helmet>
-      {/* <RegisterForm /> */}
+
       <SignUp />
     </main>
   );
