7e55611dff8a0428c458af2374e016b82a9867c7
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-16 21:54:10 +0300

    Update contacts.jsx

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -5,9 +5,21 @@ import { Filter } from '../components/filter/filter';
 import { selectContacts } from '../redux/contacts/selectors';
 import { useEffect } from 'react';
 import { fetchContacts } from '../redux/contacts/operations';
-import css from '../components/App.module.css';
+// import css from '../components/App.module.css';
 import { Helmet } from 'react-helmet';
-
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const theme = createTheme({
+  typography: {
+    h4: {
+      color: '#1976d2',
+      fontWeight: '700',
+      textAlign: 'center',
+    },
+    // Disable h3 variant
+    // h1: undefined,
+  },
+});
 export default function ContactsPage() {
   const dispatch = useDispatch();
 
@@ -18,23 +30,28 @@ export default function ContactsPage() {
   }, [dispatch]);
 
   return (
-    <main className={css.container}>
-      <Helmet>
-        <title>Contacts</title>
-      </Helmet>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      {Number(contacts.length) > 1 && <Filter />}
+    <ThemeProvider theme={theme}>
+      <main>
+        <Helmet>
+          <title>Contacts</title>
+        </Helmet>
+        <Typography component="h1" variant="h4">
+          Phonebook
+        </Typography>
+        <ContactForm />
+        <h2>Contacts</h2>
+        {Number(contacts.length) > 1 && <Filter />}
 
-      {isLoading && <p>Loading contacts...</p>}
-      {contacts.length > 0 ? (
-        <ContactList />
-      ) : (
-        <p>Your phonebook is empty. Please add contact.</p>
-      )}
-    </main>
+        {isLoading && <p>Loading contacts...</p>}
+        {contacts.length > 0 ? (
+          <ContactList />
+        ) : (
+          <p>Your phonebook is empty. Please add contact.</p>
+        )}
+      </main>
+    </ThemeProvider>
   );
 }
 //contacts={contacts}
 // export default ContactsPage;
+// className={css.container}
