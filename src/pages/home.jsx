126d2a68335baf2c5193e131c563ad678f9ba42e
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-17 02:57:39 +0300

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,3 @@
-// const styles = {
-//   container: {
-//     minHeight: 'calc(100vh - 50px)',
-//     display: 'flex',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   title: {
-//     fontWeight: 500,
-//     fontSize: 48,
-//     textAlign: 'center',
-//     color: 'black',
-//   },
-// };
 import { Helmet } from 'react-helmet';
 export default function HomePage() {
   return (
