3d53e07c04c45423587bbc57cadc4e3b71a1a49e
TkachAn <4ndrey.tkachenko@gmail.com>
2022-10-16 17:36:49 +0300

    Update register.jsx

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,14 +1,15 @@
 import { Helmet } from 'react-helmet';
-import { RegisterForm } from '../components/forms/register';
-
+// import { RegisterForm } from '../components/forms/register';
+import SignUp from 'components/forms/singUp';
 export default function Register() {
   return (
     <main>
-      <h3>Registration</h3>
+      {/* <h3>Registration</h3> */}
       <Helmet>
         <title>Registration</title>
       </Helmet>
-      <RegisterForm />
+      {/* <RegisterForm /> */}
+      <SignUp />
     </main>
   );
 }
