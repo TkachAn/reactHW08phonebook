db5ff9e39b5ce2b2c22c36930472ef718ab2c75d
TkachAn <4ndrey.tkachenko@gmail.com>
2023-07-19 18:02:32 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,27 +26,9 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route
-          path="/register"
-          element={
-            <RestrictedRoute
-              redirectTo="/contacts"
-              component={<RegisterPage />}
-            />
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
-          }
-        />
-        <Route
-          path="/contacts"
-          element={
-            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
-          }
-        />
+        <Route path="/register" element={ <RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} /> }/>
+        <Route  path="/login" element={ <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />}/>
+        <Route path="/contacts" element={<PrivateRoute redirectTo="/login" component={<ContactsPage />} />} />
         <Route path="*" element={<HomePage />} />
       </Route>
     </Routes>
